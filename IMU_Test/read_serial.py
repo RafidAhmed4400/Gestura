771a5b95c6bf09a3b89342858a827f21edb98fb9
Meep <othermeep23@gmail.com>
2026-05-03 20:49:16 -0400

    gesture data with gravity

--- IMU_Test/read_serial.py
+++ IMU_Test/read_serial.py
@@ -6,13 +6,13 @@ start = time.time()
 
 ser = serial.Serial('/dev/tty.usbserial-110', 115200)  # or 'COM#' for windows
 
-with open("N_.csv", "w") as f:
-    f.write("Blank, g_x, g_y, g_z, a_x, a_y, a_z, x, y, z, w, pinky, ring, middle, index, thumb, index\n")
-    count = -1
+with open("gesture_data_gravity/Z/Z_50.csv", "w") as f:
+    f.write("Blank, g_x, g_y, g_z, a_x, a_y, a_z, grav_x, grav_y, grav_z, x, y, z, w, pinky, ring, middle, index, thumb, tick\n")
+    count = -2
     while True:
         line = ser.readline().decode().strip()
         if count > 0:
             f.write(line + ", " + str(count) + "\n")
         count += 1
-        if time.time() - start >= 16:
+        if time.time() - start >= 1.5:
             break
